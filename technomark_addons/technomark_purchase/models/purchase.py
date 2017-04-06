075f7d96a9854a5697c77ae5dc9455256a44e664
kiran <kirana@redbytes.in>
2017-04-06 15:54:57 +0530

    To add modification for set approx weight to POL on chnage of product id

--- technomark_addons/technomark_purchase/models/purchase.py
+++ technomark_addons/technomark_purchase/models/purchase.py
@@ -5,6 +5,17 @@ from odoo import api, fields, models
 from num2words import num2words
 import datetime
 
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from odoo.tools.float_utils import float_is_zero, float_compare
+from odoo.exceptions import UserError, AccessError
+from odoo.tools.misc import formatLang
+from odoo.addons.base.res.res_partner import WARNING_MESSAGE, WARNING_HELP
+import odoo.addons.decimal_precision as dp
+
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
     """
@@ -32,6 +43,44 @@ class PurchaseOrderLine(models.Model):
     """ Inherit for Adding new fields and inherit functionality to calculate subtotal
         as per product weight
     """
+
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        """ Inherit function to pass approx weight to POL on change of product id"""
+        result = {}
+        if not self.product_id:
+            return result
+
+        # Reset date, price and quantity since _onchange_quantity will provide default values
+        self.date_planned = datetime.today().strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+        self.price_unit = self.product_qty = 0.0
+        self.product_uom = self.product_id.uom_po_id or self.product_id.uom_id
+        ## LOgic to set weight to POL on chnage of product id and if weight set is true
+        if self.product_id.is_weight_applicable:
+            self.approx_weight = self.product_id.weight or 0.0
+        result['domain'] = {'product_uom': [('category_id', '=', self.product_id.uom_id.category_id.id)]}
+
+        product_lang = self.product_id.with_context({
+            'lang': self.partner_id.lang,
+            'partner_id': self.partner_id.id,
+        })
+        self.name = product_lang.display_name
+        if product_lang.description_purchase:
+            self.name += '\n' + product_lang.description_purchase
+
+        fpos = self.order_id.fiscal_position_id
+        if self.env.uid == SUPERUSER_ID:
+            company_id = self.env.user.company_id.id
+            self.taxes_id = fpos.map_tax(self.product_id.supplier_taxes_id.filtered(lambda r: r.company_id.id == company_id))
+        else:
+            self.taxes_id = fpos.map_tax(self.product_id.supplier_taxes_id)
+
+        self._suggest_quantity()
+        self._onchange_quantity()
+
+        return result
+
+
 
     @api.multi
     def write(self, vals):
@@ -97,4 +146,4 @@ class PurchaseOrderLine(models.Model):
     ## Add new fields on PO line
     serial_no = fields.Integer(compute='_get_po_line_seq', string="Sr.No")
     material = fields.Char(string="Material")
-    approx_weight = fields.Float(related="product_id.weight", string="Approx Weight(kg)")
+    approx_weight = fields.Float(string="Approx Weight(kg)")
