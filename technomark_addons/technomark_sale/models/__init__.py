e3d9b64862f44dc4bd716df4bee0b8d53c5e04c9
kiran <kirana@redbytes.in>
2017-04-13 17:29:14 +0530

    TO Hide this to remove name_get functionality to show product name and description in name field (Not required for now)

--- technomark_addons/technomark_sale/models/__init__.py
+++ technomark_addons/technomark_sale/models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import sale_order
+# Hide this to remove name_get functionality to show product name and description in name field (Not required for now)
 # import product
 import stock_picking
 import res_partner
