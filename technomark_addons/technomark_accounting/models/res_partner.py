056240110ddd665a941253c9a9ffc7e431ea898b
kiran <kirana@redbytes.in>
2017-04-17 18:16:26 +0530

    To assign partner tin to all contacts for partner

--- technomark_addons/technomark_accounting/models/res_partner.py
+++ technomark_addons/technomark_accounting/models/res_partner.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     partner_vat_tin = fields.Char(string="VAT TIN")
+
+
+    @api.multi
+    def write(self, vals):
+        """ Logic for to assign vat tin to partners all child contacts"""
+        for child in self.child_ids:
+            child.partner_vat_tin = vals['partner_vat_tin']
+        res = super(ResPartner, self).write(vals)
+        return res
