85707aaa38b7a89b4d22ee13435c4e3f74ca74f7
kiran <kirana@redbytes.in>
2017-04-20 17:05:26 +0530

    To add product id onchange changes bom components

--- technomark_addons/technomark_mrp/models/mrp_bom.py
+++ technomark_addons/technomark_mrp/models/mrp_bom.py
@@ -7,6 +7,18 @@ from odoo import api, fields, models
 class MrpBomLine(models.Model):
     _inherit = "mrp.bom.line"
 
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        """ Inherit onchange function to pass material, size , etc values
+        from product form while selecting product on BOM line"""
+        if self.product_id:
+            self.product_uom_id = self.product_id.uom_id.id
+            ## Pass all values from prodcut form onchange of product id
+            self.material = self.product_id.material
+            self.machine_drawing_no = self.product_id.machine_drawing_no
+            self.casting_drawing_no = self.product_id.casting_drawing_no
+            self.size = self.product_id.size
+
     @api.multi
     def _get_line_seq(self):
         """ This funcion generate PO line serial number sequence
@@ -24,5 +36,3 @@ class MrpBomLine(models.Model):
     material = fields.Char(string="Material")
     machine_drawing_no = fields.Char(string="Machine Drawing")
     casting_drawing_no = fields.Char(string="Casting Drawing")
-    # Inherit field to remove deciaml point and convert to integer
-    # product_qty = fields.Integer(string='Product Quantity', default=1, required=True)
