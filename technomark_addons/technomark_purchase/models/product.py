e747dcdc39945d705e16992da4ae7176edc4dfc5
kiran <kirana@redbytes.in>
2017-06-13 16:48:59 +0530

    To invisible variant field from bom line and resolve product costing issue

--- technomark_addons/technomark_purchase/models/product.py
+++ technomark_addons/technomark_purchase/models/product.py
@@ -10,3 +10,10 @@ class ProductTemplate(models.Model):
     """
     # This field allow to use product weight in Sale order Line
     is_weight_applicable = fields.Boolean(string="Allow To Use Weight")
+
+    @api.one
+    @api.depends('property_cost_method', 'categ_id.property_cost_method')
+    def _compute_cost_method(self):
+        """ Inherit this Method to apply cost method from product category only"""
+        #self.cost_method = self.property_cost_method or self.categ_id.property_cost_method
+        self.cost_method = self.categ_id.property_cost_method
